94f36de3ae3e047896d9a1f4ea5186d775ae419f
Margrad <marcogranada@gmail.com>
2019-01-25 23:18:42 +0000

    Empty project

---
+++ BattleBalls/Source/BattleBalls/BattleBallsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleBallsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEBALLS_API ABattleBallsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
